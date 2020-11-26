816d815c1e783fee266e2603736ca5c0acb4ffca
LucasCarranzaBertarelli <66894042+LucasCarranzaBertarelli@users.noreply.github.com>
2020-11-26 12:38:00 -0300

    Merge branch 'main' of https://github.com/agustinaA-Lopez/LogicGame into main

---
+++ Assets/Textos/Tiempo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tiempo : MonoBehaviour
+{
+    public static float tiempoRespuesta = 30;
+
+    public GameObject Puntos;
+    public static int points=0;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        tiempoRespuesta -= Time.deltaTime;
+        GetComponent<TextMesh>().text = ((int)tiempoRespuesta).ToString();
+        if ((int)tiempoRespuesta == 0) tiempoRespuesta = 30;
+        if (MainController.respuesta != 0) tiempoRespuesta = 29;
+
+        //hace que no empiece el tiempo hasta que salga del inicio
+        if (MainController.nivel < 1)
+        {
+            GetComponent<TextMesh>().text = "";
+            tiempoRespuesta = 29;
+        }
+        if (MainController.nivel > 0)
+        {
+            Puntos.GetComponent<TextMesh>().text = ((int)points). ToString();
+
+        }
+
+
+
+    }
+}
